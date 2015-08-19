337e0ced825c8289a7bad4d48ea84dd11127edf6
Muthu <muthukrishnan.suresh1987@gmail.com>
2015-08-18 20:30:56 -0700

    Added ng route code

--- admin-ui/app/configuration/app.js
+++ admin-ui/app/configuration/app.js
@@ -2,5 +2,15 @@
  * Created by nithya_r04 on 16/08/15.
  */
 (function(){
-    var app=angular.module('app',['workmanagement.common']);
+    var app=angular.module('app',['ng-route','workmanagement.common']);
+    app.config(['$routeProvider'],function($routeProvider){
+        $routeProvider.
+            when('/', {
+                templateUrl: 'login/login.html',
+                controller: 'LoginController'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    });
 })();
