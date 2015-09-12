446344018e987989e5b14393d34a92e1cc055a0e
Nithya Priya Radhakrishnan <nithyarad@gmail.com>
2015-09-12 16:31:39 -0700

    signup service changes

--- admin-services/request-handler/MongoDB.js
+++ admin-services/request-handler/MongoDB.js
@@ -11,7 +11,7 @@
             if (!err) {
                 console.log("We are connected");
             }
-            var collection = db.collection("batch_document_insert_collection_safe");
+            var collection = db.collection("master_userlist");
             switch (mode){
                 case 'login':
                     resData = authorizeLogin(collection, req,res, db);
@@ -25,19 +25,70 @@
     }
     function authorizeLogin(collection, req,res, db){
         var fetchUser = {};
-        collection.findOne({emailId: req.emailId, Password: req.password}, function (err, item) {
-            fetchUser = item;
-            res.send(fetchUser);
-            db.close();
+        collection.findOne({emailId: req.emailId}, function (err, item) {
+            if(!assert.equal(null, err)){
+                if(assert.equal(item.emailId, req.emailId)){
+                    if(assert.equal(item.password, req.password)){
+                        fetchUser = {
+                            status: "success",
+                            username: item.username,
+                            userid: item._id
+                        };
+                        res.send(fetchUser);
+                        db.close();
+                    }
+                }else{
+                    //emailId is invalid
+                    var resObj = {
+                        "status": "failure",
+                        "err_msg": "EmailId/Password is invalid"
+                    };
+                    res.send(resObj);
+                    db.close();
+                }
+            }else if (err){
+                var resObj = {
+                    "status": "fail",
+                    "err_msg": "Unexpected Service Failure",
+                    "err_field": "login"
+                };
+                res.send(resObj);
+                db.close();
+            }
             return;
         });
     }
+
     function createUser(collection, req, db){
-        // Insert a single document
-        collection.insert([{emailId: req.emailId, Password: req.password, userName: req.username}], function (err, result) {
-            assert.equal(null, err);
-            db.close();
-            return;
-        })
+        //duplicate check
+        collection.findOne({emailId: req.emailId}, function (err, item) {
+            if(!assert.equal(null, err)){
+                var resObj = {
+                    "status": "fail",
+                    "err_msg": "emailId already exists",
+                    "err_field": "emailId"
+                };
+                res.send(resObj);
+                db.close();
+
+            }else{
+                // Insert a single document
+                collection.insert([req], function (err, result) {
+                    if(!assert.equal(null, err)){
+                        res.send({"status": "success"});
+                        db.close();
+                    }else if (err){
+                        var resObj = {
+                            "status": "fail",
+                            "err_msg": "Unexpected Service Failure",
+                            "err_field": "login"
+                        };
+                        res.send(resObj);
+                        db.close();
+                    }
+                })
+            }
+        });
+        return;
     }
 })();
