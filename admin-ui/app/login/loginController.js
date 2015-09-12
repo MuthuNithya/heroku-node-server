5eb01e43419ab2ede608d720c2f2e5087f52c9a5
Muthu <muthukrishnan.suresh1987@gmail.com>
2015-09-11 23:13:15 -0700

    login code changes

--- admin-ui/app/login/loginController.js
+++ admin-ui/app/login/loginController.js
@@ -2,13 +2,23 @@
 "use strict";
     angular.module('workmanagement.login').controller('loginController',['$scope',function($scope){
     var loginCtrl=this;
+        loginCtrl.signInData={
+                            "userName":"",
+                            "passWord":""
+                            };
+        loginCtrl.signUpData={
+                            "userName":"",
+                            "emailID":"",
+                            "passWord":"",
+                            "confirmPassword":""
+        };
         loginCtrl.submitForm = function(formMode){
             if(formMode === 'signup') {
                 loginCtrl.formSignUp.$submitted = true;
             } else {
                 loginCtrl.formSignIn.$submitted = true;
             }
-            console.log(formMode);
+            console.log(loginCtrl.signUpData);
         };
     }]);
 })();
