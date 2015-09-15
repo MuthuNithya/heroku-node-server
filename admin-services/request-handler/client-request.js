6bc63d4bab6aecc33549022f4488654c5ad8650a
MuthuNithya <muthukrishnan.suresh1987@gmail.com>
2015-09-15 06:58:07 +0000

    code

--- admin-services/request-handler/client-request.js
+++ admin-services/request-handler/client-request.js
@@ -3,6 +3,7 @@
     var app = express();
     var repoConnect = require('./MongoDB.js');
     var bodyParser = require('body-parser');
+    var WebSocketServer = require('ws').Server;
     // Create application/x-www-form-urlencoded parser
     app.use(bodyParser.json());       // to support JSON-encoded bodies
     app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
@@ -20,8 +21,19 @@
         repoConnect.authorizeLogin('signup', req.body, res);
     });
     var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+    var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
     var server = require('http').createServer(app);
+    wss = new WebSocketServer({
+        server: server,
+        autoAcceptConnections: false
+    });
+    wss.on('connection', function(ws) {
+      console.log("New connection");
+      ws.on('message', function(message) {
+        ws.send("Received: " + message);
+      });
+      ws.send('Welcome!');
+    });
     console.log(" on " + server_ip_address + ", server_port " + server_port)
     server.listen(server_port, server_ip_address, function(){
       console.log("Listening on " + server_ip_address + ", server_port " + server_port)
