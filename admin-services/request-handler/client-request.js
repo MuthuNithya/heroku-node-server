0fc571a483dd237ae7fee9197c083818be1308d4
MuthuNithya <muthukrishnan.suresh1987@gmail.com>
2015-09-15 05:48:32 +0000

    changed port no

--- admin-services/request-handler/client-request.js
+++ admin-services/request-handler/client-request.js
@@ -19,7 +19,7 @@
     app.post('/auth_signup', function(req, res){
         repoConnect.authorizeLogin('signup', req.body, res);
     });
-    var server = app.listen(8081, function () {
+    var server = app.listen(3000, function () {
         var host = server.address().address;
         var port = server.address().port;
         console.log("Example app listening at http://%s:%s", host, port)
