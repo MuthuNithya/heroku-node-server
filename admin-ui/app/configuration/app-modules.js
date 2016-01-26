04a9c1ec2e826b292c9df4e34d9f547afbb1e9bd
Muthu <muthukrishnan.suresh1987@gmail.com>
2016-01-25 18:50:30 -0800

    History page UI changes

--- admin-ui/app/configuration/app-modules.js
+++ admin-ui/app/configuration/app-modules.js
@@ -5,6 +5,6 @@
         angular.module('workmanagement.dashboard', ['ngCookies','chart.js']);
         angular.module('workmanagement.logout', []);
         angular.module('workmanagement.create',['ngCookies','ui.grid','ui.grid.edit','ui.grid.cellNav','ui.grid.selection']);
-        angular.module('workmanagement.history',['ngCookies','ui.grid','ui.grid.edit','ui.grid.cellNav','ui.grid.selection']);
+        angular.module('workmanagement.history',['ngCookies','ui.grid','ui.grid.pagination','ui.grid.selection']);
 
 })();
--- admin-ui/app/history/historyService.js
+++ admin-ui/app/history/historyService.js
@@ -18,42 +18,46 @@
             });
             return true;
         };
-        historyserv.data =[
-            {Date: "01/01/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/02/2016",TotalHoursEntered: 7,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/03/2016",TotalHoursEntered: 9,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/04/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/05/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/06/2016",TotalHoursEntered: 5,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/07/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/08/2016",TotalHoursEntered: 10,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/09/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/10/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/11/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/12/2016",TotalHoursEntered: 9,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/13/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/14/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/15/2016",TotalHoursEntered: 5,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/16/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/17/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/18/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/19/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/20/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/21/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/22/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/23/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
-            {Date: "01/24/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'}
-    ];
 
         historyserv.initHistoryTableGrid = function(){
             var gridOptions;
             gridOptions= {
-                data: historyserv.data,
-                enablePaging: true,
-                pagingOptions: historyserv.pagingOptions,
-                showFooter: true,
-                filterOptions: historyserv.filterOptions
+                paginationPageSizes: [20, 40, 60],
+                paginationPageSize: 20,
+                columnDefs: [
+                    { name: 'Date' },
+                    { name: 'TotalHoursEntered' },
+                    { name: 'Status' },
+                    { name: 'LastModifiedDate' }
+                ]
             }
+            gridOptions.data =[
+                {Date: "01/01/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/02/2016",TotalHoursEntered: 7,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/03/2016",TotalHoursEntered: 9,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/04/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/05/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/06/2016",TotalHoursEntered: 5,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/07/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/08/2016",TotalHoursEntered: 10,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/09/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/10/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/11/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/12/2016",TotalHoursEntered: 9,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/13/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/14/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/15/2016",TotalHoursEntered: 5,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/16/2016",TotalHoursEntered: 8,Status: "Saved", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/17/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/18/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/19/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/20/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/21/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/22/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/23/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'},
+                {Date: "01/24/2016",TotalHoursEntered: 0,Status: "Yet to complete", LastModifiedDate: '01/05/2016 14:00'}
+            ];
+
             return gridOptions;
         };
         return historyserv;
