9491c15d7f4db7c85c98ba4aa0682c890d801d10
Nithya Priya Radhakrishnan <nithyarad@gmail.com>
2016-02-06 02:28:54 -0800

    cell edit condition changes

--- admin-ui/app/create/createService.js
+++ admin-ui/app/create/createService.js
@@ -47,6 +47,9 @@
             });
             return deferred.promise;
         };
+        function cellEditCondition($scope){
+            return !$scope.grid.appScope.isReadMode;
+        }
         createserv.initCreateTableGrid = function() {
             var gridOptions;
             return gridOptions = {
@@ -57,9 +60,6 @@
                 enableRowSelection: true,
                 enableSelectAll: false,
                 enableFiltering: false,
-                cellEditableCondition:function($scope){
-                    return !$scope.grid.appScope.isReadMode;
-                },
                 exporterSuppressColumns : [ '_fromTime','_toTime' ],
                 columnDefs: [
                     {
@@ -68,7 +68,8 @@
                         resizable: false,
                         width: '25%',
                         cellClass:'fromTime',
-                        editableCellTemplate:'<input type="text" class="time" jquery-timepicker="" ng-model="row.entity._fromTime" ng-change="grid.appScope.createCtrl.mapFromTime(grid.appScope.createCtrl.selectedDate,row.entity,row.entity._fromTime);"/>'
+                        editableCellTemplate:'<input type="text" class="time" jquery-timepicker="" ng-model="row.entity._fromTime" ng-change="grid.appScope.createCtrl.mapFromTime(grid.appScope.createCtrl.selectedDate,row.entity,row.entity._fromTime);"/>',
+                        cellEditableCondition: cellEditCondition
                     },
                     {
                         field: '_toTime',
@@ -76,7 +77,8 @@
                         resizable: false,
                         width: '25%',
                         cellClass:'toTime',
-                        editableCellTemplate:'<input type="text" class="time" jquery-timepicker="" ng-model="row.entity._toTime" ng-change="grid.appScope.createCtrl.mapToTime(grid.appScope.createCtrl.selectedDate,row.entity,row.entity._toTime);"/>'
+                        editableCellTemplate:'<input type="text" class="time" jquery-timepicker="" ng-model="row.entity._toTime" ng-change="grid.appScope.createCtrl.mapToTime(grid.appScope.createCtrl.selectedDate,row.entity,row.entity._toTime);"/>',
+                        cellEditableCondition: cellEditCondition
                     },
                     {
                         field: 'description',
@@ -86,7 +88,8 @@
                         type: 'string',
                         width: '45%',
                         cellClass:'effortDesc',
-                        editableCellTemplate:'<input type="text" class="description" ng-model="row.entity.description"/>'
+                        editableCellTemplate:'<input type="text" class="description" ng-model="row.entity.description"/>',
+                        cellEditableCondition: cellEditCondition
                     }
                 ]};
         };
